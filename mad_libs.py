b4f7f84bec9b75ae62791e96385ae5460341c661
siko408 <gamerbloxhd@gmail.com>
2019-08-29 23:00:52 -0700

    Now you can choose Terminal text color

--- mad_libs.py
+++ mad_libs.py
@@ -1,74 +1,133 @@
 #import nltk
 #import random
 #nltk.download('wordnet')
-#from nltk.corpus import wordnet
-print(" Today we will talk about [Noun] who is currently living with [Noun].\n Being together has been a positive impact on their [adverb]\n")
-x = input("If you try adding less words it will be replaced with defaults\n, no more then 3 words will be used:\n")
-words = []
-new_word = []
-sentence = ["walking", "across", "swim", "San Francisco", "California", "Bay Area", "Riding", "bike", "run", "house", "pick", "change", "unlock", "love", "girlfriend"]
-def clense(y): #seperates words into individual strings
-    word = ""
-    l = []
-    for x in range(0, len(y)):
-        if(y[x] != " "):
-            l.append(y[x])
-        elif(y[x] == " " and y[x-1] != "."):
-            l.append("|")
-    for j in range(0, len(l)):
-        if(l[j] != "|"):
-            word += l[j]
-            print(l[j])
-        elif(l[j] == "|"):
-            words.append(word)
-            word = ""
-    words.append(word)
-    size = len(words)
-    print(words)
-    g = 0
-    for x in words:   #Second filter until I fix first or use .pop() 8/29/19
-        if x == "":
+#from nltk.corpus import wordne
 
-            print("detected")
-        else:
-            new_word.append(x)
-        g += 1
+from colorama import Fore, Back, Style
+
+check = False
+color_choice = []
+arr = []
+dic = {"BLACK": Fore.BLACK, "RED": Fore.RED, "GREEN": Fore.GREEN, "YELLOW": Fore.YELLOW, "BLUE": Fore.BLUE, "MAGENTA": Fore.MAGENTA, "CYAN": Fore.CYAN, "WHITE": Fore.WHITE}
+key = "WHITE"
+while(check is False):
+    if len(color_choice) != 0:
+        key = color_choice[len(color_choice) - 1]
+        print(key)
+        print(dic.get(key, "WHITE") + "Today we will talk about [Noun] who is currently living with [Noun].\n Being together has been a positive impact on their [adverb]\n")
+    print(dic.get(key, "WHITE") + " Today we will talk about [Noun] who is currently living with [Noun].\n Being together has been a positive impact on their [adverb]\n")
+    x = input(dic.get(key, "WHITE") + "If you try adding less words it will be replaced with defaults\n, no more then 3 words will be used:\n")
+    words = []
+    new_word = []
+    sentence = ["walking", "across", "swim", "San Francisco", "California", "Bay Area", "Riding", "bike", "run", "house", "pick", "change", "unlock", "love", "girlfriend"]
+    def clense(y): #seperates words into individual strings
+        word = ""
+        l = []
+        for x in range(0, len(y)):
+            if(y[x] != " "):
+                l.append(y[x])
+            elif(y[x] == " " and y[x-1] != "."):
+                l.append("|")
+        for j in range(0, len(l)):
+            if(l[j] != "|"):
+                word += l[j]
+                print(l[j])
+            elif(l[j] == "|"):
+                words.append(word)
+                word = ""
+        words.append(word)
+        size = len(words)
+        print(words)
+        g = 0
+        for x in words:   #Second filter until I fix first or use .pop() 8/29/19
+            if x == "":
+                print("detected")
+            else:
+                new_word.append(x)
+                g += 1
+                print(new_word)
+    clense(x)
+
     print(new_word)
-clense(x)
+    userWords = len(new_word)
+    pcWords = len(sentence)
+    greater = 0
+    least = 0
+    if(pcWords > userWords):
+        greater = pcWords
+        least = userWords
+    else:
+        greater = userWords
+        least = pcWords
+    value = []
+    if(userWords >= 3):
+        for x in range(0, 3):
+            value.append(new_word[x])
+    else:
+        mark = len(new_word)
+        for x in range(mark, 3):
+            new_word.append(None)
+        for x in new_word:
+            value.append(x)
+    for x in value:
+        if(x is None):
+            x = "NULL"
+    val = value[0]
+    val2 = value[1]
+    val3 = value[2]
 
-print(new_word)
-userWords = len(new_word)
-pcWords = len(sentence)
-greater = 0
-least = 0
-if(pcWords > userWords):
-    greater = pcWords
-    least = userWords
-else:
-    greater = userWords
-    least = pcWords
-value = []
-if(userWords >= 3):
-    for x in range(0, 3):
-        value.append(new_word[x])
-else:
-    mark = len(new_word)
-    for x in range(mark, 3):
-        new_word.append(None)
-    for x in new_word:
-        value.append(x)
-for x in value:
-    if(x is None):
-        x = "NULL"
-val = value[0]
-val2 = value[1]
-val3 = value[2]
-
-sentence_choice = f""" Today we will talk about {val} who is currently living with {val2}.\n Being together has been a positive impact on their {val3}"""
+    sentence_choice = dic.get(key, "WHITE") + f""" Today we will talk about {val} who is currently living with {val2}.\n Being together has been a positive impact on their {val3}"""
 # for x in range(0,random.randint(0,greater)):
 # sentence_choice += sentence[random.randint(0,len#(sentence) - 1)] + " " +  words[random.randint(0,len(words)# - 1)].upper() + " "
-
-print(sentence_choice)
+    print(sentence_choice)
+    choose = False
+    choose2 = True
+    while(choose == False):
+        print(dic.get(key, "WHITE") + """ TYPE S TO STOP, TYPE R TO RESTART PROGRAM, TYPE C FOR COLOR TERMINAL OR D FOR DEFAULT SETTINGS,   \n IF YOU CHOOSE D THIS WILL PROMPT AGAIN\n """)
+        an = input().upper()
+        print(an)
+        if an == 'S':
+            check = True
+            choose = True
+        elif an == 'R':
+            print(dic.get(key, "WHITE") + "restarting")
+            choose = True
+        elif an == 'C':
+            choose = True
+            choose2 = False
+        elif an == 'D':
+            key = "WHITE"
+            color_choice = []
+        else:
+            print("YOU DID NOT TYPE ANYTHING OR ANYTHING RELEVANT, NONE CASE SENSTITIVE")
+        while(choose2 is False):
+            color = input(dic.get(key, "WHITE") + """Type desired color story text, the list consit of:\n BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE \n""").upper()
+            if color == "BLACK":
+                color_choice.append(color)
+                choose2 = True
+            elif color == "RED":
+                color_choice.append(color)
+                choose2 = True
+            elif color == "GREEN":
+                color_choice.append(color)
+                choose2 = True
+            elif color == "YELLOW":
+                color_choice.append(color)
+                choose2 = True
+            elif color == "BLUE":
+                color_choice.append(color)
+                choose2 = True
+            elif color == "MAGENTA":
+                color_choice.append(color)
+                choose2 = True
+            elif color == "CYAN":
+                color_choice.append(color)
+                choose2 = True
+            elif color == "WHITE":
+                color_choice.append(color)
+                choose2 = True
+            else:
+                print(dic.get(key, "WHITE") + "YOU DID NOT CHOOOSE!!, \n", """ TYPE S TO STOP, TYPE R TO RESTART PROGRAM, TYPE C FOR COLOR TERMINAL OR D for default settings\n  """)
 #if not wordnet.synsets("patrick"):
 #    print("not a word")
 #else:
